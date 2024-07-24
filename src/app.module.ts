77e7e26a7472a0be568cd94a4dc973d201e5408e
Tejasdavande07 <tejas.davande.dev.iic@gmail.com>
2024-07-24 19:48:13 +0530

    update : event model 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketModule } from './socket/socket.module';
+import { EventModule } from './events/event.module';
+
 
 @Module({
-  imports: [SocketModule],
+  imports: [SocketModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
